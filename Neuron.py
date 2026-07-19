7bc2e3eb1e220e63591947c18d38fa7accc59f99
Paweł <ptomala1223@gmail.com>
2026-07-19 17:50:32 +0200

    NN early structure

---
+++ Neuron.py
@@ -0,0 +1,46 @@
+import numpy as np
+import math
+
+class Token:
+    def __init__(self, weights : [], bias : int, fun : str):
+        self.weights = np.array(weights)
+        self.bias = bias
+        self.fun = fun
+        self.activationFuncions = {
+            "relu" : self.relu,
+            "sigmoid" : self.sigmoid,
+        }
+
+
+    def __str__(self):
+        return "Token"
+
+    def __len__(self):
+        return len(self.weights)
+
+    def __eq__(self, other):
+        return (
+                np.array_equal(self.weights, other.weights)
+                and self.bias == other.bias
+                and self.fun == other.fun
+        )
+
+    def __call__(self, x : []):
+        return self.forward(x)
+
+    def sigmoid(self, Z):
+        return 1 / (1 + math.exp(-Z))
+
+    def relu(self, Z):
+        return np.maximum(0, Z)
+
+    def forward(self, inputs : []):
+        inputs = np.array(inputs)
+
+        if len(inputs) != len(self.weights):
+            return False
+
+        Z = np.sum(self.weights * inputs) + self.bias
+        output = self.activationFuncions[self.fun](Z)
+        return output
+
